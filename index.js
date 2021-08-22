096bcef85f07b64c76da65739c2fe8b76a471b9a
srinivasan <srini3720@gmail.com>
2021-08-22 19:04:08 +0530

    production

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const winston = require("winston");
 const express = require("express");
 const app = express();
 
-
-
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
