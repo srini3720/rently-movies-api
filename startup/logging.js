64ecef13e829bfacd95d93dc93cd227537d1c924
SRINIVASAN <SRINI3720@GMAIL.COM>
2021-08-26 11:50:05 +0530

    log in db

--- startup/logging.js
+++ startup/logging.js
@@ -1,10 +1,11 @@
 const winston = require("winston");
 require("winston-mongodb");
 require("express-async-errors");
+require("dotenv").config();
 
 module.exports = function () {
   winston.add(winston.transports.File, { filename: "logfile.log" });
-  winston.add(winston.transports.MongoDB, { db: "mongodb://localhost/vidly" });
+  winston.add(winston.transports.MongoDB, { db: process.env.db });
 
   winston.handleExceptions(
     // new winston.transports.Console({ colorize: true, prettyPrint: true }),
